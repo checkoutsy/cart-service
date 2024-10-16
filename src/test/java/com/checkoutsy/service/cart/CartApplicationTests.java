f8cec98072e897ea58194e97bdb046f040783226
Matheus Sousa <matheus.sousa@omnisaude.app>
2024-10-16 02:07:27 -0300

    feat: initial commit

---
+++ src/test/java/com/checkoutsy/service/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkoutsy.service.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
